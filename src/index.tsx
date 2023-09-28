b84b20000a81dc5dfef6a4248e7f29f933b62d67
Namjeongsu <godenbel@naver.com>
2023-09-29 02:33:55 +0900

    아폴로,axios,react-cookie,sass,mui,라우터,eslint,prettier 적용 끝

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { ApolloProvider } from '@apollo/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {client} from "./api/apollo/client";
-import {ApolloProvider} from "@apollo/client";
+import { client } from './api/apollo/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <ApolloProvider client={client}>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    </ApolloProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ApolloProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
