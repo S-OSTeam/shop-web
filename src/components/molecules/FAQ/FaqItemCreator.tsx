bddeac4f3e054c2fa3b003be77d279c91bdfc960
i dont know who i am plz help me i ate my own orgasm <69139078+Noma9363@users.noreply.github.com>
2023-10-10 16:40:51 +0900

    :wrench: PR 테스트
    User 기준 UI
    - FAQ 메인 사이트 싱글페이지 디자인
    - FaqItemCreator.tsx 의 파라미터로 true를 받을 경우 해당 컴포넌트 스타일을 변경함으로써 활성화 (보안에 취약할 수 있음)
    - FaqItemCreator.tsx 내부 text-field mui 인풋 입력을 통해 recoil > faqItemList.ts 내부 아톰에 저장 후
    useEffect를 통해 페이지 리로드 혹은 특정 상황 마운트 시 해당 아이템을 mpa으로 랜더링함
    
    :wrench: 추가할 부분
    쿼리, 쿠키, 로그인, 인증 기능을 통해 스웨거로 전달한 후 데이터 받아와서 렌더하기

--- src/components/molecules/FAQ/FaqItemCreator.tsx
+++ src/components/molecules/FAQ/FaqItemCreator.tsx
@@ -1,68 +1,145 @@
-import { ChangeEvent, useRef, useState } from 'react';
+/* eslint-disable */
+import React, { ChangeEvent, useRef, useState } from 'react';
 import { useRecoilState, useSetRecoilState } from 'recoil';
+import {
+    Box,
+    Button,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    Input,
+    InputLabel,
+    TextField,
+    TextFieldProps,
+    Typography,
+} from '@mui/material';
+import { TextareaAutosize } from '@mui/base/TextareaAutosize';
 import { FaqItem, faqListState } from '../../../recoil/atoms/FaqItemList';
-import { FormControl, Input, InputLabel } from '@mui/material';
+import '../../../styles/scss/FaqCreatorStyles.scss';
 
 interface myProps {
+    isActive: boolean;
 }
-
-export const FaqItemCreator = () => {
+export const FaqItemCreator = ({ ...props }: myProps) => {
     /*상태*/
     // faq atom
     // const [faqItem, setFaqItem] = useRecoilState<FaqItem>();
-    // faq_title
-    const [inputValTitle, setInputValTitle] = useState<string>("");
-    // faq_content
-    const [inputValContext, setInputValContext] = useState<string>("");
 
-    /*
-    * 위에 받고
-    * 리코일로 저장하고 초기화
-    *
-    * */
-
-    /*
-    * 리코일 상태관리
-    * 컴포넌트 값 변경시 리랜더링됨
+    /*const [inputs, setInputs] = useState({
+        title: '',
+        context: '',
+    });
+    const { title, context } = inputs;
     */
-    const setFaqList = useSetRecoilState(faqListState)
+    // textField input 제목
+    const [inputTitle, setInputTitle] = useState<string>('');
+    // textField input 내용
+    const [inputContext, setInputContext] = useState<string>('');
+
+    /*
+     * 위에 받고
+     * 리코일로 저장하고 초기화
+     *
+     * */
+
+    /*
+     * 리코일 상태관리
+     * 컴포넌트 값 변경시 리랜더링됨
+     */
+    const setFaqList = useSetRecoilState(faqListState);
 
     /*서버 다운되었으니 useRef 넘버랑 스트링 합치기*/
     const renderNum = useRef(1);
     // 시간 구하는 메서드
 
-    /*메서드*/
+    // render boolean
+    let isRender: boolean = props.isActive;
+
+    /* 메서드 */
+
+    /* setRecoil */
+    const setFaqItem = useSetRecoilState(faqListState);
+
+    // onSubmit
     const addItem = () => {
-        setFaqList((preventList)=>[
+        if (!inputTitle) {
+            alert('제목을 입력해 주세요');
+            return;
+        }
+        if (!inputContext) {
+            alert('내용을 입력해 주세요');
+            return;
+        }
+        setFaqList((preventList) => [
             ...preventList,
             {
-                faq_id: (renderNum + "userid"),
-                fgr_id: "",
-                faq_title: "",
-                faq_content: "",
-                faq_datetime: "",
-                faq_ip: "",
-            }
-        ])
+                faq_id: renderNum + 'userid',
+                fgr_id: '',
+                faq_title: '',
+                faq_content: '',
+                faq_datetime: '',
+                faq_ip: '',
+            },
+        ]);
     };
-    // 제목 입력값
-    const onChangeTitle = (e:ChangeEvent<HTMLInputElement>) => {
-        setInputValContext(e.target.value);
-    }
-
-
-    // 내용 입력값
-
-    return(
-        <>
-            <FormControl>
-                <InputLabel htmlFor="inputTitle">문의제목</InputLabel>
-                <Input
-                    id="inputTitle"
-                    onChange={onChangeTitle}
-
-                />
-            </FormControl>
-        </>
+    return (
+        <Box className={`faq-creator render-${isRender.toString()}`} id="faqCreator">
+            {props.isActive && (
+                <form>
+                    <Typography component="h2" variant="h5" gutterBottom>
+                        궁금하신 사항이 있나요?
+                    </Typography>
+                    <Typography component="h3" variant="h6" gutterBottom>
+                        빠르게 답변해 드리겠습니다.
+                    </Typography>
+                    <FormControl className="faq-area title">
+                        <InputLabel htmlFor="inputTitle" />
+                        <TextField
+                            id="inputTitle"
+                            className="mui-faq-input title"
+                            label="제목"
+                            value={inputTitle}
+                            onChange={(e) => {
+                                setInputTitle(e.target.value);
+                            }}
+                            placeholder="제목 기입란"
+                            required
+                        />
+                    </FormControl>
+                    <FormControl className="faq-area context">
+                        <InputLabel htmlFor="inputContext" />
+                        <TextField
+                            id="inputContext"
+                            className="mui-faq-input context"
+                            label="내용"
+                            value={inputContext}
+                            onChange={(e) => {
+                                setInputContext(e.target.value);
+                            }}
+                            multiline
+                            placeholder="내용 기입란"
+                            required
+                            rows={6}
+                        />
+                    </FormControl>
+                    <FormGroup className="faq-area form-group-check">
+                        <FormControlLabel control={<Checkbox />} label="답변 완료시 SNS 수신 동의" />
+                        <FormControlLabel control={<Checkbox />} label="답변 완료시 메일 수신 동의" />
+                        <FormControlLabel control={<Checkbox />} label="익명으로 문의하기" />
+                    </FormGroup>
+                    <Button variant="text" id="submit-btn">
+                        문의하기
+                    </Button>
+                </form>
+            )}
+        </Box>
     );
-}
+};
+
+/*
+ * 섹션 분리하기
+ * 문의제목
+ * 내용
+ *
+ * */
--- src/components/organisms/FaqComponentOff.tsx
+++ src/components/organisms/FaqComponentOff.tsx
@@ -1,63 +1,84 @@
 /* eslint-disable */
 import React, { useState } from 'react';
 import { Box, Button, Typography } from '@mui/material';
+import { FaqItemCreator } from '../molecules/FAQ/FaqItemCreator';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { faqListState } from '../../recoil/atoms/FaqItemList';
+
+const QuestState = {
+    false: '문의 하기',
+    true: '문의 내역 작성중',
+};
+
 /*
-* gql스웨거 사용이 불가능한 상태로 오프라인으로 작업해야됨
-*/
+ * gql스웨거 사용이 불가능한 상태로 오프라인으로 작업해야됨
+ */
 export const FaqComponentOff = () => {
+    const pageIs = {};
+
     /* 토큰 */
     // const accessToken = `Bearer ${getCookie('accessToken')}`;
+    /* 리코일값 가져오기 */
+    const [faqEle, setFaqEle] = useRecoilState(faqListState);
 
     /* 상태 */
+    /* 리코일 내부 아톰의 값을 가져오고 컴포넌트를 출력하는 메서드 만들기 */
+    // useEffect로 특정조건 랜딩처리
+    const getFaqData = () => {};
+
+    // 버튼 클릭 이벤트 -> 참: faqCreator On
     const [isAct, setIsAct] = useState<boolean>(false);
 
     /* 메소드 */
-    const onOpenCompo = () => {
-        setIsAct(prevState => !prevState);
-    }
-
+    const handleOnClick = () => {
+        setIsAct((current) => !current);
+        console.log(isAct);
+    };
     return (
-        <div className='page-faq-main'>
+        <div className="page-faq-main">
             <Typography variant={'h1'}>또 터졌나요?</Typography>
             <Typography variant={'h2'}>또 뭔 짓거리를 하신 겁니까</Typography>
             <div>
                 <Typography variant={'h3'}>문의해보세요</Typography>
-                <Box sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'middle',
-                    fontSize: '16px',
-                    padding: '5px 10px',
-                }}>
+                <Box
+                    className="faq-page-main"
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'middle',
+                        fontSize: '16px',
+                        padding: '5px 10px',
+                    }}
+                >
                     <Button
+                        onClick={handleOnClick}
                         sx={{
-                            width: "40%",
-                            color:'white',
+                            width: '40%',
+                            color: 'white',
                             borderColor: 'white',
                             backgroundColor: 'black',
-                            ":hover":{
+                            ':hover': {
                                 borderColor: 'black',
                                 color: 'black',
                                 backgroundColor: 'white',
-                            }
+                            },
+                            ':active': {
+                                backgroundColor: '#2c99e3',
+                                color: 'white',
+                            },
                         }}
-                        className='faq-btn'
-                        size='medium'
+                        className={isAct ? `.faq-btn-active` : `.faq-btn-inactive`}
+                        size="medium"
                         variant={'outlined'}
-                    >문의하기
+                    >
+                        {isAct ? '문의 내역 작성중...' : '문의 하기'}
                     </Button>
                 </Box>
-                {
-                    isAct &&
-                }
-
+                <FaqItemCreator isActive={isAct} />
             </div>
             <div>
-
                 <Typography variant={'h3'}>문의내역</Typography>
-                {
-
-                }
+                {}
             </div>
         </div>
     );
