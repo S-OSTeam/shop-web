a58bfac8d04d1354256d06f5d66edafe662ba0c8
JooHui-void <elis100@naver.com>
2024-01-10 20:38:40 +0900

    fix: storybook eslint fixing

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,8 +20,10 @@ module.exports = {
         'import/extensions': 0,
         'no-use-before-define': 0,
         'import/no-unresolved': 0,
+        'react/no-unescaped-entities': 0,
         'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
         'no-shadow': 0,
+        'react/jsx-props-no-spreading': 'off',
         'react/prop-types': 0,
         'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
         'jsx-a11y/no-noninteractive-element-interactions': 0,
