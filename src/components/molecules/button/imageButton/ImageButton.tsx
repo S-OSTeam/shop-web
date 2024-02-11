5897e77070dcb015d6575cb735717230f3b6c974
cywin1018 <cywin0821@gmail.com>
2024-02-11 11:17:32 +0900

    chore : login

--- src/components/molecules/button/imageButton/ImageButton.tsx
+++ src/components/molecules/button/imageButton/ImageButton.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import clsN from 'classnames';
+import Image from '@atoms/source/image/Image';
 import Button from '@atoms/button/Button';
+import style from './style/ImageButton.module.scss';
 
 export interface ImageBtnProps {
     className?: string;
-    img: React.ReactNode;
+    imgClsN?: string;
+    imgPath: string;
+    alt?: string
 }
 
-const ImageButton = ({ className, img }: ImageBtnProps) => {
-    return <Button className={clsN(className)}>{img}</Button>;
-};
 
+const ImageButton = (
+    {
+        className,
+        imgClsN,
+        imgPath,
+        alt
+    }:ImageBtnProps) => {
+    return(
+
+        <Button
+            className={clsN(className, `${style.imgButton}`)}
+        >
+            <Image
+                className={clsN(imgClsN, `${style.img}`)}
+                imgPath={imgPath}
+                alt={alt}
+            />
+        </Button>
+    )
+};
+ImageButton.propTypes = {
+    ...Image.propTypes,
+    imgClsN:  PropTypes.string,
+    className: PropTypes.string,
+};
+ImageButton.defaultProps = {
+    ...Image.defaultProps,
+    className: `${style.btnIcon}`,
+}
 export default ImageButton;
