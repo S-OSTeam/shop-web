a94282d21c67c79235f4eba63c71f4e831bf7db3
cywin1018 <cywin0821@gmail.com>
2024-02-17 22:16:49 +0900

    chore : mobile login page create

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 import LoginForm from '@components/templates/LoginForm/LoginForm';
+import MobileLoginPage from './MobileLoginPage';
 // import LinearProgress from '@atoms/progress/LinesrProgress/LinearProgress';
 
 const LoginPage = () => {
-    return (
-        <Box component="div">
-            <LoginForm />
-        </Box>
-    );
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    return <Box component="div">{isMobile ? <MobileLoginPage /> : <LoginForm />}</Box>;
 };
 
 export default LoginPage;
