49d4ceaef0900a11962179dd8fc5e2bf3b9e1306
cywin1018 <cywin0821@gmail.com>
2024-02-19 15:22:08 +0900

    chore : mobile screen update

--- src/components/organisms/login/login/MobileLogin.tsx
+++ src/components/organisms/login/login/MobileLogin.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Box } from '@mui/material';
 // import { useNavigate } from 'react-router-dom';
-import Text from '@components/atoms/text/Text';
 import Button from '@components/atoms/button/Button';
 // import axios from 'axios';
 import clsN from 'classnames';
+import DividerWithText from '@components/molecules/divider/dividerWithText/DividerWithText';
 import style from './style/style.module.scss';
 
 const MobileLogin = () => {
@@ -36,13 +36,16 @@ const MobileLogin = () => {
 
     return (
         <Box className={clsN(`${style['mobile-login-wrapper']}`)}>
-            <Text
-                text="또는"
-                variant="subtitle2"
-                className={clsN(`${style['mobile-login-wrapper__title']}`)}
-                align="center"
-            />
-
+            <Box className={clsN(`${style['mobile-login-wrapper__title']}`)}>
+                <DividerWithText
+                    text="또는"
+                    variant="h6"
+                    align="center"
+                    className={clsN(`${style['mobile-login-wrapper__text']}`)}
+                >
+                    또는
+                </DividerWithText>
+            </Box>
             <Box className={clsN(`${style['mobile-login-wrapper__btn-wrapper']}`)}>
                 <Button
                     className={clsN(`${style['mobile-login-wrapper__btn-wrapper__signup-btn']}`)}
