39432980f23633fa01cd47c9a89f4d3c34bee727
cywin1018 <cywin0821@gmail.com>
2024-02-21 14:38:13 +0900

    fix : 주석 제거 및 custom.d.ts
    주석처리 해놓은것 다 빼고, custom.d.ts로 svg 파일 되도록 수정했음

--- src/components/organisms/login/login/MobileLogin.tsx
+++ src/components/organisms/login/login/MobileLogin.tsx
@@ -8,32 +8,6 @@ import DividerWithText from '@components/molecules/divider/dividerWithText/Divid
 import style from './style/style.module.scss';
 
 const MobileLogin = () => {
-    // const handleLogin = async () => {
-    //     console.log('Login Data:', formData);
-    //     try {
-    //         const response = await axios.post(`${BaseUrl}/user/login`, formData);
-    //         console.log(response);
-    //         if (response.status === 200) {
-    //             const { accessToken, refreshToken } = response.data;
-
-    //             setCookie('accessToken', `${accessToken}`, {
-    //                 path: '/',
-    //                 sameSite: 'strict',
-    //             });
-
-    //             setCookie('refreshToken', `${refreshToken}`, {
-    //                 path: '/',
-    //                 sameSite: 'strict',
-    //             });
-    //             console.log('Login Success');
-    //             navigate('/phone');
-    //         }
-    //     } catch (error) {
-    //         console.error('Login Error:', error);
-    //     }
-    // };
-    // const navigate = useNavigate();
-
     return (
         <Box className={clsN(`${style['mobile-login-wrapper']}`)}>
             <Box className={clsN(`${style['mobile-login-wrapper__title']}`)}>
