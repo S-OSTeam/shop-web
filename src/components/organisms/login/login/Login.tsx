b8d7d42cb1e0e0c7f1b57323cdfa64f1d5e1870f
cywin1018 <cywin0821@gmail.com>
2024-02-22 15:36:10 +0900

    fix : saveId component
    scss 및 구조 수정

--- src/components/organisms/login/login/Login.tsx
+++ src/components/organisms/login/login/Login.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { Box } from '@mui/material';
 import Text from '@components/atoms/text/Text';
 import Input from '@components/atoms/input/Input';
+import SaveId from '@components/molecules/checkbox/saveId/SaveId';
 import Button from '@components/atoms/button/Button';
 import { useDomSizeCheckHook } from '@hooks/useDomSizeCheck.hook';
 import clsN from 'classnames';
