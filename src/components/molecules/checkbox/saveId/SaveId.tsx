d3c86d61ccd6268fa582d1573f9d5cd92d10d614
cywin1018 <cywin0821@gmail.com>
2024-02-22 15:36:10 +0900

    fix : saveId component
    scss 및 구조 수정

--- src/components/molecules/checkbox/saveId/SaveId.tsx
+++ src/components/molecules/checkbox/saveId/SaveId.tsx
@@ -4,9 +4,13 @@ import { Box, Checkbox } from '@mui/material';
 import clsN from 'classnames';
 import style from './style/saveId.module.scss';
 
-const SaveId = () => {
+interface saveIdProps {
+    className: string;
+}
+
+const SaveId = ({ className }: saveIdProps) => {
     return (
-        <Box className={clsN(`${style['save-id']}`)}>
+        <Box className={clsN(className, `${style['save-id']}`)}>
             <Checkbox />
             <Text className={clsN(`${style['save-id__text']}`)} text="아이디 저장" />
         </Box>
