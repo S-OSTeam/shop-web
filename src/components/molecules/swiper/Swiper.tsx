f0365ae469a87c85e15cddd6269cae65b36f8702
dmltn <dmltn3426@naver.com>
2024-02-25 21:45:37 +0900

    refactor: swiper 변경

--- src/components/molecules/swiper/Swiper.tsx
+++ src/components/molecules/swiper/Swiper.tsx
@@ -6,6 +6,7 @@ import clsN from 'classnames';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+// import Image from '@atoms/source/image/Image';
 import styles from './style/Swiper.module.scss';
 
 interface CustomSwiperProps {
@@ -27,12 +28,16 @@ const Swiper = ({ ...props }: CustomSwiperProps) => {
         clickable: true,
     };
     const autoPlay = { delay: props.delay, disableOnInteraction: false };
+    const navigation = {
+        prevEl: '.prev',
+        nextEl: '.next',
+    };
     return (
         <div>
             <CustomSwiper
                 className={clsN(props.className, styles.swiper)}
                 pagination={props.isPagination ? pagination : false}
-                navigation={props.isNavigation}
+                navigation={props.isNavigation ? navigation : false}
                 spaceBetween={props.spaceBetween}
                 autoplay={props.isAutoPlay ? autoPlay : false}
                 loop
@@ -46,6 +51,32 @@ const Swiper = ({ ...props }: CustomSwiperProps) => {
                         <img className={clsN(props.imgClsN, styles.img)} alt={props.alt} src={imgPath} />
                     </SwiperSlide>
                 ))}
+                <svg
+                    className="prev"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M16.876 2.363a1.239 1.239 0 0 0-1.752 0l-8.761 8.76a1.239 1.239 0 0 0 0 1.753l8.76 8.761a1.239 1.239 0 1 0 1.753-1.752L8.991 12l7.885-7.885a1.239 1.239 0 0 0 0-1.752Z"
+                        fill="currentColor"
+                    />
+                </svg>
+                <svg
+                    className="next"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M6.363 21.637a1.239 1.239 0 0 0 1.752 0l8.761-8.76a1.239 1.239 0 0 0 0-1.753l-8.76-8.761a1.239 1.239 0 1 0-1.753 1.752L14.248 12l-7.885 7.885a1.239 1.239 0 0 0 0 1.752Z"
+                        fill="currentColor"
+                    />
+                </svg>
             </CustomSwiper>
         </div>
     );
@@ -71,7 +102,7 @@ Swiper.defaultProps = {
     className: styles[''],
     slideClsN: styles[''],
     imgClsN: styles[''],
-    spaceBetween: 50,
+    spaceBetween: 0,
     isNavigation: false,
     isPagination: false,
     isAutoPlay: false,
