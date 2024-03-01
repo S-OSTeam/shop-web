9e565244636f73f81afabdc752287fcd53faacca
dmltn <dmltn3426@naver.com>
2024-03-01 17:58:46 +0900

    feature: home swiper-pagination 커스텀 적용 및 freeMode 추가

--- src/components/molecules/swiper/Swiper.tsx
+++ src/components/molecules/swiper/Swiper.tsx
@@ -1,66 +1,62 @@
 import React from 'react';
 import { Swiper as CustomSwiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation, Autoplay } from 'swiper/modules';
-import { Box } from '@mui/material';
+import { Pagination, Navigation, Autoplay, FreeMode } from 'swiper/modules';
+import ImageButton from '@molecules/button/imageButton/ImageButton';
+import { PaginationOptions } from 'swiper/types/modules/pagination';
+import { NavigationOptions } from 'swiper/types/modules/navigation';
 import PropTypes from 'prop-types';
-import * as ReactDOMServer from 'react-dom/server';
 import clsN from 'classnames';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
 import styles from './style/Swiper.module.scss';
 
 interface CustomSwiperProps {
-    className?: string;
-    slideClsN?: string;
-    imgClsN?: string;
-    items: string[];
+    className?: string; // 전체 swiper className
+    slideClsN?: string; // swiper slide className
+    btnClsN?: string; // ImageButton의 Button className
+    imgClsN?: string; // ImageBUtton의 Image className
+    items: string[]; // 아이템의 이미지 주소
     alt?: string;
-    onChange?: () => void;
-    spaceBetween?: number;
-    isPagination?: boolean;
-    isNavigation?: boolean;
-    isAutoPlay?: boolean;
-    delay?: number;
-    paginationClsN?: string;
+    onChange?: () => void; // slide 넘길때 발생하는 onChange 옵션
+    spaceBetween?: number; // 각 slide 간격
+    slidesPerView?: number; // 한 화면에 출력하는 총 slide의 갯수
+    isPagination?: PaginationOptions | boolean; // pagination 사용 여부 및 커스텀 pagination
+    isNavigation?: NavigationOptions | boolean; // navigation 사용 여부 및 커스텀 navigation
+    isAutoPlay?: boolean; // auto play 사용 여부
+    isFreeMode?: boolean; // freeMode 사용 여부
+    isLoop?: boolean; // loop 사용 여부
+    delay?: number; // auto play 사용 시 delay
 }
 
 const Swiper = ({ ...props }: CustomSwiperProps) => {
-    const pagination = {
-        clickable: true,
-        type: 'fraction',
-        formatFractionCurrent: (number: number) => {
-            return `${number}`;
-        },
-        formatFractionTotal: (number: number) => {
-            return `${number}`;
-        },
-        renderFraction: (currentClass: string, totalClass: string) => {
-            return ReactDOMServer.renderToStaticMarkup(
-                <Box className={clsN(props.paginationClsN)}>
-                    <span className={`${currentClass}`} /> / <span className={`${totalClass}`} />
-                </Box>,
-            );
-        },
-    };
     const autoPlay = { delay: props.delay, disableOnInteraction: false };
     return (
         <div>
             <CustomSwiper
-                className={clsN(props.className, styles.swiper)}
-                pagination={props.isPagination ? pagination : false}
+                className={clsN(props.className, styles['swiper-wrapper'])}
+                slidesPerView={props.slidesPerView}
+                pagination={props.isPagination}
                 navigation={props.isNavigation}
                 spaceBetween={props.spaceBetween}
                 autoplay={props.isAutoPlay ? autoPlay : false}
-                loop
+                freeMode={props.isFreeMode}
+                loop={props.isLoop}
                 loopedSlides={1}
-                modules={[Pagination, Navigation, Autoplay]}
+                modules={[Pagination, Navigation, Autoplay, FreeMode]}
                 onSlideChange={props.onChange}
             >
                 {props.items.map((imgPath, index) => (
                     // eslint-disable-next-line react/no-array-index-key
-                    <SwiperSlide className={clsN(props.slideClsN, styles['swiper__swiper-slide'])} key={index}>
-                        <img className={clsN(props.imgClsN, styles.img)} alt={props.alt} src={imgPath} />
+                    <SwiperSlide className={clsN(props.slideClsN, styles['swiper-wrapper__swiper-slide'])} key={index}>
+                        <ImageButton
+                            className={clsN(props.btnClsN, styles.btn)}
+                            imgClsN={clsN(props.imgClsN, styles.img)}
+                            alt={props.alt}
+                            imgPath={imgPath}
+                        />
                     </SwiperSlide>
                 ))}
             </CustomSwiper>
@@ -71,16 +67,19 @@ const Swiper = ({ ...props }: CustomSwiperProps) => {
 Swiper.propTypes = {
     className: PropTypes.string,
     slideClsN: PropTypes.string,
+    btnClsN: PropTypes.string,
     imgClsN: PropTypes.string,
     items: PropTypes.arrayOf(PropTypes.string),
     alt: PropTypes.string,
     onChange: PropTypes.func,
     spaceBetween: PropTypes.number,
+    slidesPerView: PropTypes.number,
     isNavigation: PropTypes.bool,
     isPagination: PropTypes.bool,
     isAutoPlay: PropTypes.bool,
+    isFreeMode: PropTypes.bool,
+    isLoop: PropTypes.bool,
     delay: PropTypes.number,
-    paginationClsN: PropTypes.string,
 };
 
 Swiper.defaultProps = {
@@ -88,13 +87,16 @@ Swiper.defaultProps = {
     alt: '이미지가 없습니다.',
     className: styles[''],
     slideClsN: styles[''],
+    btnClsN: styles[''],
     imgClsN: styles[''],
     spaceBetween: 0,
+    slidesPerView: 1,
     isNavigation: false,
     isPagination: false,
     isAutoPlay: false,
+    isFreeMode: false,
+    isLoop: true,
     delay: 5000,
-    paginationClsN: styles[''],
 };
 
 export default Swiper;
--- src/components/organisms/home/product/recommend/Recommend.tsx
+++ src/components/organisms/home/product/recommend/Recommend.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import { Item } from '@util/test/interface/Item';
 import clsN from 'classnames';
 import Text from '@atoms/text/Text';
+import Swiper from '@molecules/swiper/Swiper';
 import styles from './styles/Recommand.module.scss';
 
 interface RecommendProps {
@@ -10,12 +11,21 @@ interface RecommendProps {
 }
 
 const Recommend = ({ recommendItem }: RecommendProps) => {
+    const items = recommendItem.map((item) => {
+        return item.imageUrls[0];
+    });
     return (
         <Box className={clsN(styles['recommend-wrapper'])}>
             <Text className={clsN(styles['recommend-wrapper__title'])} text="추천 카테고리" />
-            {recommendItem.map((item) => (
-                <Box className={clsN(styles['recommend-wrapper__category-btn'])}>{item.title}</Box>
-            ))}
+            <Swiper
+                items={items}
+                btnClsN={clsN(styles['recommend-wrapper__category-btn'])}
+                imgClsN={clsN(styles['recommend-wrapper__category-img'])}
+                isFreeMode
+                spaceBetween={8}
+                slidesPerView={4}
+                isLoop={false}
+            />
         </Box>
     );
 };
