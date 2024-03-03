e45c740f270bc8eda2b0ae93c402514bea0f9016
cywin1018 <cywin0821@gmail.com>
2024-03-03 19:40:18 +0900

    fix : mocules
    molcues로 재사용성 확장

--- src/components/molecules/button/imageButton/ImageButton.tsx
+++ src/components/molecules/button/imageButton/ImageButton.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import clsN from 'classnames';
 import Button from '@atoms/button/Button';
-import style from './style/ImageButton.module.scss';
 
 export interface ImageBtnProps {
     className?: string;
-    children: React.ReactNode;
+    img: React.ReactNode;
 }
 
-const ImageButton = ({ className, children }: ImageBtnProps) => {
-    return <Button className={clsN(className, `${style.imgButton}`)}>{children}</Button>;
+const ImageButton = ({ className, img }: ImageBtnProps) => {
+    return <Button className={clsN(className)}>{img}</Button>;
 };
-ImageButton.propTypes = {
-    className: PropTypes.string,
-};
-ImageButton.defaultProps = {
-    className: `${style.btnIcon}`,
-};
+
 export default ImageButton;
