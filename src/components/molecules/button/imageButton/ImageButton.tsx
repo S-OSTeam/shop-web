d8450a6783b9b3311126d0e0cdf18f21788df70d
cywin1018 <cywin0821@gmail.com>
2024-03-03 19:40:18 +0900

    fix : mocules
    molcues로 재사용성 확장

--- src/components/molecules/button/imageButton/ImageButton.tsx
+++ src/components/molecules/button/imageButton/ImageButton.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsN from 'classnames';
-import Image from '@atoms/source/image/Image';
 import Button from '@atoms/button/Button';
 
 export interface ImageBtnProps {
