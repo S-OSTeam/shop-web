a0a4f76ad4f4726ade51933c8daaf4c6c0ef58d8
i dont know who i am plz help me i ate my own orgasm <noma9363@gmail.com>
2024-03-07 21:03:38 +0900

    tab 관련 컴포넌트 제작 + scss 관련 변수 추가
    font-size : rem 을 활용하여 root를 기준으로 활용
    font-weight 그리고 breakpoint map 변수 추가

--- src/util/test/data/itemNavCsResponse.ts
+++ src/util/test/data/itemNavCsResponse.ts
@@ -1,6 +1,27 @@
 export const ItemNavCsResponse = [
-    '고객센터',
-    '서비스 도움말',
-    '비즈니스 관련',
-    '회사소개',
+    {
+        publicId: 1,
+        categoryPublicId: 1,
+        title: '고객센터',
+        url: 'www.naver.com',
+    },
+    {
+        publicId: 1,
+        categoryPublicId: 1,
+        title: '서비스 도움말',
+        url: 'www.naver.com',
+    },
+    {
+        publicId: 1,
+        categoryPublicId: 1,
+        title: '비즈니스 관련',
+        url: 'www.naver.com',
+    },
+    {
+        publicId: 1,
+        categoryPublicId: 1,
+        title: '회사소개',
+        url: 'www.naver.com',
+    },
+
 ];
