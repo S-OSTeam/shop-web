715a1e5392297ee17af091080b04d7516dd8edd3
i dont know who i am plz help me i ate my own orgasm <noma9363@gmail.com>
2024-03-07 21:03:38 +0900

    tab 관련 컴포넌트 제작 + scss 관련 변수 추가
    font-size : rem 을 활용하여 root를 기준으로 활용
    font-weight 그리고 breakpoint map 변수 추가

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import CustomerService from '@routes/cs/CustomerService';
 import DeamHome from './home/DeamHome';
 import Product from './product/Product';
 import Login from './login/Login';
@@ -12,6 +13,7 @@ export const AppRouter = () => {
             <Route path="/product" element={<Product />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/cs" element={<CustomerService/>} />
         </Routes>
     );
 };
