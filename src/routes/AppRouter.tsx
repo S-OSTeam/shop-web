6c44e0e3db359cff135fcbd42c65f8b920df797b
i dont know who i am plz help me i ate my own orgasm <69139078+Noma9363@users.noreply.github.com>
2024-04-05 13:25:51 +0900

    머지 테스트
    머지 테스트 입니디.
    몰큘과 올가니즘 포함

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import DeamHome from '@pages/home/DeamHomePage';
 import Product from '@pages/product/ProductPage';
 import Header from '@components/layout/header/Header';
+import CustomerServicePage from '@pages/cs/CustomerServicePage';
+import AdminPage from '@pages/admin/AdminPage';
 
 export const AppRouter = () => {
     return (
@@ -11,6 +13,8 @@ export const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<DeamHome />} />
                 <Route path="/product" element={<Product />} />
+                <Route path="/support" element={<CustomerServicePage />} />
+                <Route path="/manager" element={<AdminPage />} />
             </Routes>
         </div>
     );
