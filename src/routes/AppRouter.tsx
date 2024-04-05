b82397ffb47383cec0dbd732019d5ab9dba5135b
i dont know who i am plz help me i ate my own orgasm <69139078+Noma9363@users.noreply.github.com>
2024-04-05 15:58:34 +0900

    대십보드 헤더 테스트
    테스트

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,16 +3,21 @@ import { Route, Routes } from 'react-router-dom';
 import DeamHome from '@pages/home/DeamHomePage';
 import Product from '@pages/product/ProductPage';
 import Header from '@components/layout/header/Header';
+import CustomerServicePage from '@pages/cs/CustomerServicePage';
+import AdminPage from '@pages/admin/AdminPage';
 import Login from '@pages/login/LoginPage';
 
 export const AppRouter = () => {
     return (
-        <Layout>
+        <div>
+            <Header />
             <Routes>
                 <Route path="/" element={<DeamHome />} />
                 <Route path="/product" element={<Product />} />
+                <Route path="/support" element={<CustomerServicePage />} />
+                <Route path="/manager" element={<AdminPage />} />
                 <Route path="/login" element={<Login />} />
             </Routes>
-        </Layout>
+        </div>
     );
 };
