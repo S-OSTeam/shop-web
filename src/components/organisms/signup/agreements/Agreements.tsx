6bb19afc3f173e0ab95de041aba3f95853760e7d
cywin1018 <cywin0821@gmail.com>
2024-04-05 17:42:33 +0900

    fix : 회원가입 페이지 수정

--- src/components/organisms/signup/agreements/Agreements.tsx
+++ src/components/organisms/signup/agreements/Agreements.tsx
@@ -8,30 +8,23 @@ import style from './style/style.module.scss';
 const Agreements = () => {
     const isInMobile = useDomSizeCheckHook(768);
 
+    const checkboxTexts = ['선택적 SNS 광고 동의 여부', '기타 고객정보 영리적 사용 등', '기타 등등'];
+
     return (
         <div>
             {isInMobile ? (
-                <Box className={clsN(`${style['agreements-wrapper']}`)}>
-                    <Divider
-                        variant="middle"
-                        textAlign="center"
-                        className={clsN(`${style['agreements-wrapper__divider']}`)}
-                    >
+                <Box className={clsN(style['agreements-wrapper'])}>
+                    <Divider variant="middle" textAlign="center" className={clsN(style['agreements-wrapper__divider'])}>
                         동의사항
                     </Divider>
-                    <Box className={clsN(`${style['agreements-wrapper__checkbox-wrapper']}`)}>
-                        <CheckboxWithText
-                            className={clsN(`${style['agreements-wrapper__checkbox-wrapper__checkbox']}`)}
-                            text="선택적 SNS 광고 동의 여부"
-                        />
-                        <CheckboxWithText
-                            className={clsN(`${style['agreements-wrapper__checkbox-wrapper__checkbox']}`)}
-                            text="기타 고객정보 영리적 사용 등"
-                        />
-                        <CheckboxWithText
-                            className={clsN(`${style['agreements-wrapper__checkbox-wrapper__checkbox']}`)}
-                            text="기타 등등"
-                        />
+                    <Box className={clsN(style['agreements-wrapper__checkbox-wrapper'])}>
+                        {checkboxTexts.map((text) => (
+                            <CheckboxWithText
+                                key={text}
+                                className={clsN(style['agreements-wrapper__checkbox-wrapper__checkbox'])}
+                                text={text}
+                            />
+                        ))}
                     </Box>
                 </Box>
             ) : (
