65f66eacbaee83a34522724325b462d49b212cb5
i dont know who i am plz help me i ate my own orgasm <noma9363@gmail.com>
2024-04-08 21:54:56 +0900

    feat: 관리자 페이지 링크 리스트 추가
    - GQL 혹은 .ts 로부터 데이터를 전달받아 링크 컴포넌트를 렌더
    - 링크 데이터 중 이중으로 데이터가 있을 경우 collapse 컴포넌트로 선택하여 두번째 컨텐츠 전용 컴포넌트를 렌더 구현
    - 아이콘 처리 과정을 swich 문이 아닌 Record 로 아이콘 컴포넌트 선택 처리

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import DeamHome from '@pages/home/DeamHomePage';
 import Product from '@pages/product/ProductPage';
 import Header from '@components/layout/header/Header';
@@ -8,9 +8,21 @@ import AdminPage from '@pages/admin/AdminPage';
 import Login from '@pages/login/LoginPage';
 
 export const AppRouter = () => {
+    // React Router 가 제공하는 useLocation 훅을 사용해서 조건부 랜더링하기
+
+    // 현재 경로
+    const currentLocation = useLocation();
+    // 특정 경로당 컴포넌트 렌더 처리하기
+    const headerProvider = () => {
+        if(currentLocation.pathname === '/manager'){
+            return null;
+        }
+        return <Header />
+    }
+
     return (
         <div>
-            <Header />
+            {headerProvider()}
             <Routes>
                 <Route path="/" element={<DeamHome />} />
                 <Route path="/product" element={<Product />} />
