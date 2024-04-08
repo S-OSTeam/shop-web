fa1292c0bb1f21db5891baecf14d877566ba48a7
i dont know who i am plz help me i ate my own orgasm <noma9363@gmail.com>
2024-04-08 21:54:56 +0900

    feat: 관리자 페이지 링크 리스트 추가
    - GQL 혹은 .ts 로부터 데이터를 전달받아 링크 컴포넌트를 렌더
    - 링크 데이터 중 이중으로 데이터가 있을 경우 collapse 컴포넌트로 선택하여 두번째 컨텐츠 전용 컴포넌트를 렌더 구현
    - 아이콘 처리 과정을 swich 문이 아닌 Record 로 아이콘 컴포넌트 선택 처리

--- src/components/molecules/link/Link.tsx
+++ src/components/molecules/link/Link.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Link as MuiLink, LinkProps as MuiLinkProps} from '@mui/material';
+import PropTypes from 'prop-types';
+import clsN from 'classnames';
+import styles from './styles/Link.module.scss';
 
-interface LinkProps {}
-const Link = ({}: LinkProps) => {
-    return <></>;
+interface LinkProps extends MuiLinkProps{
+    // 컴포넌트 root 클래스명
+    className?: string;
+    // 자식요소
+    children: React.ReactNode;
+    // href
+    href?: string;
+}
+const LinkComponent = (
+    {
+        className,
+        children,
+        href
+    }: LinkProps
+) => {
+    return (
+        <MuiLink
+            className={clsN(className, styles.link)}
+            href={href}
+        >
+            {children}
+        </MuiLink>
+    );
 };
-export default Link;
+LinkComponent.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node.isRequired,
+    href: PropTypes.string,
+}
+LinkComponent.defaultProps = {
+    className: styles.link,
+    href: undefined,
+}
+export default LinkComponent;
