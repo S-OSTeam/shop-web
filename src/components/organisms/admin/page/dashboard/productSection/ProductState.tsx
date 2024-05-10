53e26f386fa3bb97c58a38552fd3e7ccd4690b11
i dont know who i am plz help me i ate my own orgasm <69139078+Noma9363@users.noreply.github.com>
2024-05-10 13:56:03 +0900

    대시보드 페이지 수정중
    mui 속성 boxShadow 확인중
    
    대시보드가 제공하는 위젯별 데이터를 받고 출력할 수 있도록 처리

--- src/components/organisms/admin/page/dashboard/productSection/ProductState.tsx
+++ src/components/organisms/admin/page/dashboard/productSection/ProductState.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { productStateAtom, productStateType, slotType } from '@recoil/atoms/cs/productStateAtom';
 import { Stack } from '@mui/material';
@@ -7,71 +6,57 @@ import Text from '@atoms/text/Text';
 import clsN from 'classnames';
 import styles from './styles/OrderSection.module.scss';
 
-// item 인터페이스
-interface ProductItemProps {
-    state: string;
-    product: slotType;
-}
-
 interface OrderSectionProps {
     // root 클래스명
     className?: string;
 }
 
-export const ProductState = (
-    {
-        className
-    }:OrderSectionProps
-) => {
-
+export const ProductState = ({ className }: OrderSectionProps) => {
     /* 상태 */
-    // 상품 현황 리코일 값 읽기
-    const currentProductState = useRecoilValue(productStateAtom);
-
-    /* 유기체 영역이 데이터 리코일 가져와서 사용하기 */
-    const [productState] = React.useState<productStateType>(currentProductState);
-
-
-
-    // 전시중
-    // onDisplay: number;
-    // 전시전
-    // exhibition: number;
-    // 비활성화
-    // inactive: number;
+    const productState = useRecoilValue<productStateType>(productStateAtom);
+    /* 함수 */
+    /// 파라미터 키값과 value 를 읽어들어 컴포넌트 반환
+    const renderItems = (key: string, value: slotType) => {
+        // 키값 콘솔 조회
+        console.log(`key : ${key}`);
+        // value 조회
+        const { name, amount, date } = value;
+        return (
+            <Stack
+                className={clsN(styles['item-state'])}
+                borderRadius={1}
+                boxShadow={3}
+                gap={1}
+                justifyContent="space-between"
+            >
+                <Stack className={clsN(styles['item-state__stack'])} direction="row" justifyContent="space-between">
+                    <Text text={name} className={clsN(styles['item-state__stack__title'])} />
+                    <Text text={`${amount}건`} className={clsN(styles['item-state__stack__amount'])} />
+                </Stack>
+                <Text text={`마지막 업데이트 ${date}`} className={clsN(styles['item-state__date'])} />
+            </Stack>
+        );
+    };
+    /// 오브젝트 체크
+    // 객체 자체의 enumerable 속성 [key, value] 쌍의 배열을 반환
+    // ??? for...in 루프와 다른점은 프로토타입 체인의 속성도 열거함
+    // Object.entries(productState) === 순환1({ 키값 : slotType 데이터들}) 로 구성
+    const renderItemDataProvider = Object.entries(productState).map(([key, value]) => {
+        // Object.entries 를 통해 순환중인 { 키값, 데이터:slotType } 를 renderItems(키, 밸류) 파라미터를 전달받고 JSX 컴포넌트를 리턴함
+        return renderItems(key, value);
+    });
 
     /* JSX 컴포넌트 */
-    // 항목의 제목과 건
-/*
-    const currentState = <Stack className={clsN(styles.root__stack)}>
-        <Text text={onDisplay.toString()} />
-        <Text text={exhibition.toString()} />
-    </Stack>
-*/
-    // 마지막 갱신일
-    // const latestDate = <Text text={inactive.toString()} className={clsN(styles.root__date)}/>
 
-    /* 함수 */
-    // map 처리하면서 컴포넌트 리턴
+    // useEffect
 
-    const ProductItem = ({state, product}: ProductItemProps) => {
-        return(
-            <Stack className={clsN()}>
-                <Stack className={clsN()}>
-                    <Text text={product.name}/>
-                    <Text text={`${product.amount}`}/>
-                </Stack>
-                <Text text={product.date}/>
-            </Stack>
-        )
-    }
-
-
-    ProductItem();
     // 렌더
-    return(
-        <Stack direction='column' className={clsN(styles.root, className)} boxShadow={1}>
-            ffsf
+    return (
+        <Stack direction="column" className={clsN(styles.root, className)} boxShadow={3} borderRadius={1}>
+            <Text text="상품현황" variant="h4" className={clsN(styles.root__headline)} />
+            <Stack className={clsN(styles['root__item-wrapper'])} alignItems="stretch" gap={1}>
+                {renderItemDataProvider}
+            </Stack>
         </Stack>
-    )
-}
+    );
+};
