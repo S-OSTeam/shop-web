9bd497820dbf7eb7e39da3bc777b5564d2e82c7a
i dont know who i am plz help me i ate my own orgasm <noma9363@gmail.com>
2024-06-17 13:38:30 +0900

    collapsed 와 selectbox 컴포넌트 수정
    feature :
    
    - collapsed 는 2 일반버전과 Table 전용 컴포넌트 두가지로 만들 예정,
    - selectBox 컴포넌트 제작중
    
    - GQL 을 통한 데이터 처리를 하기 전 더미데이터로 관리자 페이지의 공지사항 글 목록을 확인할 예정

--- src/components/molecules/collapsedList/CollapsedList.tsx
+++ src/components/molecules/collapsedList/CollapsedList.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React from 'react';
 import { Box } from '@mui/material';
 import ListItemButton from '@atoms/listItemButton/ListItemButton';
@@ -21,7 +20,7 @@ interface CollapsedListProps {
     // 콜랩스 클래스명
     collClsN?: string;
     // 버튼 레이블 (내용)
-    primary: React.ReactNode
+    primary: React.ReactNode;
     // 첫번째 리스트 아이콘
     listIcon?: React.ReactNode;
     // 체크 속성
@@ -29,7 +28,6 @@ interface CollapsedListProps {
     // 클릭 이벤트
     onClick?: React.MouseEventHandler<HTMLDivElement>;
     // 리스트 primary 텍스트 클릭 이벤트
-    onPrimaryClick?: ()=> void;
     // 콜랩스 내부 컨텐츠
     innerChildren: React.ReactNode;
     // 콜랩스 내부 리스트 클래스 명
@@ -40,42 +38,40 @@ interface CollapsedListProps {
     listItemRootClsN?: string;
 }
 
-const CollapsedList = (
-    {
-        className,
-        isOpen,
-        onClick,
-        listBtnClsN,
-        listIcon,
-        listIconClsN,
-        primary,
-        primaryClsN,
-        innerChildren,
-        collClsN,
-        collListClsN,
-        listItemRootClsN,
-        onPrimaryClick
-    }:CollapsedListProps
-) => {
-
-    return(
-        <Box component='div' className={clsN(className, styles.wrapper)}>
+const CollapsedList = ({
+    className,
+    isOpen,
+    onClick,
+    listBtnClsN,
+    listIcon,
+    listIconClsN,
+    primary,
+    primaryClsN,
+    innerChildren,
+    collClsN,
+    collListClsN,
+    listItemRootClsN,
+}: CollapsedListProps) => {
+    return (
+        <Box component="div" className={clsN(className, styles.wrapper)}>
             <ListItemButton className={clsN(listBtnClsN, styles['list-btn'])} onClick={onClick}>
-                <ListItemIcon className={clsN(listIconClsN, styles['list-btn__icon'])}>
-                    {listIcon}
-                </ListItemIcon>
-                <ListItemText primary={primary} className={clsN(listItemRootClsN, styles['list-btn__root'])} primaryClsN={clsN(primaryClsN, styles['list-btn__root__primary'])}/>
-                {isOpen ? <ExpandLess/> : <ExpandMore/>}
+                <ListItemIcon className={clsN(listIconClsN, styles['list-btn__icon'])}>{listIcon}</ListItemIcon>
+                <ListItemText
+                    primary={primary}
+                    className={clsN(listItemRootClsN, styles['list-btn__root'])}
+                    primaryClsN={clsN(primaryClsN, styles['list-btn__root__primary'])}
+                />
+                {isOpen ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
             <Collapse className={clsN(collClsN, styles.collapse)} isOpen={isOpen} unmountOnExit>
-                <List component='div' className={clsN(collListClsN, styles.collapse__list)} disablePadding>
+                <List component="div" className={clsN(collListClsN, styles.collapse__list)} disablePadding>
                     {innerChildren}
                 </List>
             </Collapse>
         </Box>
-    )
-}
-CollapsedList.propTypes= {
+    );
+};
+CollapsedList.propTypes = {
     // 컴포넌트 클래스명
     className: PropTypes.string,
     // 첫번째리스트 버튼 클래스명
@@ -99,17 +95,17 @@ CollapsedList.propTypes= {
     // 콜랩스 내부 컨텐츠
     innerChildren: PropTypes.node.isRequired,
     listItemRootClsN: PropTypes.string,
-}
+};
 CollapsedList.defaultProps = {
-    className: styles.wrapper, 
+    className: styles.wrapper,
     listBtnClsN: styles['list-btn'],
     primaryClsN: styles['list-btn__primary'],
     listIconClsN: styles['list-btn__icon'],
     collClsN: styles.collapse,
     collListClsN: ['collapse__list'],
-    listIcon: undefined, 
+    listIcon: undefined,
     isOpen: undefined,
     onClick: undefined,
-    listItemRootClsN: styles['']
-}
+    listItemRootClsN: styles[''],
+};
 export default CollapsedList;
