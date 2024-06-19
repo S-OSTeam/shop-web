1cd78f3e3c07c8280d6144f22cde332b62cd947e
i dont know who i am plz help me i ate my own orgasm <noma9363@gmail.com>
2024-06-19 20:22:59 +0900

    관리자 페이지 공지사항 관련 컴포넌트
    feature :
    - tableCollapsed 컴포넌트 추가
    
    collapsed 컴포넌트의 상태관리를 효율적으로 하기위해 부모가 관리함으로
    state 상태를 <boolean[]>로 하고,
    onClick 시 인덱스인 인자를 활용하여 이벤트 처리를함
    
    제너릭을 통해 받은 각 테이블의 요소들을
    renderTHeadItems... 등등 map 을 통해 새로운 요소를 받도록 설정함
    
    새로운 TSX 를 반환하는 해당 함수들을 Provider 로 전달해 모듈방식으로 분리함
    
    etc :
    - 해당 테이블 구조를 먼저 한 다음에 빠르게 GQL 관련 작업을 수행할 예정.

--- src/components/organisms/collapsedListResult/collapsedListResult.tsx
+++ src/components/organisms/collapsedListResult/collapsedListResult.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Stack } from '@mui/material';
 import clsN from 'classnames';
-import styles from './styles/CollapsedManager.module.scss';
+import styles from './styles/collapsedListResult.module.scss';
 
 // 콜랩스 매니저 인터페이스
 interface CollapsedManagerProps {
@@ -9,7 +9,7 @@ interface CollapsedManagerProps {
     className?: string;
 }
 
-const CollapsedManager = ({ className }: CollapsedManagerProps) => {
+const CollapsedListResult = ({ className }: CollapsedManagerProps) => {
     return (
         <Stack direction="column" className={clsN(className, styles.root)}>
             {/* 총 몇 건이 조회됬는지 알려주는 count 영역 */}
