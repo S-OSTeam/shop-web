fa6c5b7d2cccf585024e6384196abc7a02d45957
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-06-27 11:49:53 +0900

    chore : 네이버 로그인 기능 추가(아직 로그인 페이지가 없나?)

--- src/components/organisms/login/social/SocialLogin.tsx
+++ src/components/organisms/login/social/SocialLogin.tsx
@@ -20,6 +20,12 @@ const SocialLogin = () => {
 
     const handleNaverLogin = () => {
         console.log('NaverLogin is clicked!');
+        const NAVER_CLIENT_ID = 'Jr_uoE5CqdiK5VAarAZE';
+        const REDIRECT_URI = 'http://localhost:3000/login';
+        const STATE = false;
+        const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
+
+        window.location.href = NAVER_AUTH_URL;
     };
 
     const handleKakaoLogin = () => {
