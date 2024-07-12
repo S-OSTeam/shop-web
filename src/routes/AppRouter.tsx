0b9f4041a07e87f5939a11b348dadf8b77c4914d
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-07-12 20:42:57 +0900

    fix : AppRouter.tsx에서 경로 수정

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,9 +4,13 @@ import DeamHome from '@pages/home/DeamHomePage';
 import Product from '@pages/product/ProductPage';
 import Header from '@components/layout/header/Header';
 import CustomerServicePage from '@pages/cs/CustomerServicePage';
+import SignUp from '@routes/signup/Signup';
+import Login from '@routes/login/Login';
 import { AdminRouter } from './adminRouter/AdminRouter';
-import Login from './login/Login';
-import SignUp from './signup/Signup';
+
+
+
+
 
 
 export const AppRouter = () => {
@@ -16,11 +20,11 @@ export const AppRouter = () => {
     const currentLocation = useLocation();
     // 특정 경로당 컴포넌트 렌더 처리하기
     const headerProvider = () => {
-        if((currentLocation.pathname).includes('/manager')){
+        if (currentLocation.pathname.includes('/manager')) {
             return null;
         }
-        return <Header />
-    }
+        return <Header />;
+    };
 
     return (
         <div>
@@ -31,7 +35,7 @@ export const AppRouter = () => {
                 <Route path="/support" element={<CustomerServicePage />} />
 
                 {/* 모듈화된 관리자 페이지 라우터 렌더하기 */}
-                <Route path="/manager/*" element={<AdminRouter />}/>
+                <Route path="/manager/*" element={<AdminRouter />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
             </Routes>
