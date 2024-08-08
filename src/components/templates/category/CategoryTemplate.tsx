6e8310a675e983a8f0141846b62906e0dca6f6fa
dmltn <dmltn3426@naver.com>
2024-08-08 21:13:29 +0900

    chore: pakage.json upgrage

--- src/components/templates/category/CategoryTemplate.tsx
+++ src/components/templates/category/CategoryTemplate.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ItemCategoryTreeResponse } from '@interface/category/Category';
 import { Box } from '@mui/material';
+import { Item, ItemInterface } from '@interface/item/Item';
+import Popular from '@organisms/home/product/popular/Popular';
 
 interface CategoryTemplateProps {
     categories: ItemCategoryTreeResponse[];
+    items: ItemInterface[] | Item[] | undefined;
 }
 
-const CategoryTemplate = ({ categories }: CategoryTemplateProps) => {
+const CategoryTemplate = ({ categories, items }: CategoryTemplateProps) => {
     return (
         <Box style={{ marginTop: 80 }}>
             {categories ? (
@@ -16,6 +19,7 @@ const CategoryTemplate = ({ categories }: CategoryTemplateProps) => {
                     ))}
                 </h1>
             ) : undefined}
+            {items && <Popular popularItems={items} content="" />}
         </Box>
     );
 };
