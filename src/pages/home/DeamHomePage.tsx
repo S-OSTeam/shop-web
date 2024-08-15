ff4406c72a384195c00d88cc146b4c997408ba5b
dmltn <dmltn3426@naver.com>
2024-08-15 16:50:02 +0900

    feature util - Path url 주소가 바뀌게 된다면 한번에 수정될 수 있게 끔 string 형식으로 바꿔 두었다.

--- src/pages/home/DeamHomePage.tsx
+++ src/pages/home/DeamHomePage.tsx
@@ -4,28 +4,34 @@ import { ItemInterface } from '@util/test/interface/Item';
 import { useNavigate } from 'react-router-dom';
 import { EventInfo } from '@util/test/interface/Event';
 import { ReviewResponse } from '@interface/review/Review';
+import { Path } from '@util/Path';
 
 const DeamHomePage = () => {
     const navigation = useNavigate();
 
+    /* 메인 사용하는 홈 스와이퍼, 추천 카테고리, 이벤트 같은 모든 스와이퍼에 관한 클릭 이벤트를 실행하는 함수 이다.
+     * 카테고리와 홈 스와이퍼 같은 경우에는 아이템을 데이터로 받고 Event는 이벤트 Response가 따로 존재하기 때문에 두가지의 타입을 나누어 받았다
+     * type을 RECOMMEND와 SWIPER로 나누고 default를 SWIPER로 하고서 추천 카테고리 이벤트가 발생했을 때 RECOMMEND로 type의 값이 바뀐다.
+     * item 타입 : ItemInterface(홈 스와이퍼, 추천 카테고리) | EventInfo(이벤트)
+     * type 타입 : RECOMMEND(추천 카테고리) | SWIPER(홈 스와이퍼, 이벤트) */
     const onSwiperHandle = (item: ItemInterface | EventInfo, type: 'RECOMMEND' | 'SWIPER' = 'SWIPER') => {
         if ('id' in item) {
             const idEncode = btoa(item.id.toString()).slice(0, -1);
-            navigation(`/shop/event?id=${idEncode}`, {
+            navigation(`${Path.event}?id=${idEncode}`, {
                 state: {
                     productItem: item,
                 },
             });
         } else if (type !== 'RECOMMEND') {
             const idEncode = btoa(item.publicId.toString()).slice(0, -1);
-            navigation(`/shop/product?publicId=${idEncode}`, {
+            navigation(`${Path.product}?publicId=${idEncode}`, {
                 state: {
                     productItem: item,
                 },
             });
         } else {
             const idEncode = btoa(item.publicId.toString()).slice(0, -1);
-            navigation(`/shop/category?categoryId=${idEncode}`, {
+            navigation(`${Path.category}?categoryId=${idEncode}`, {
                 state: {
                     categoryId: item.publicId.toString(),
                 },
@@ -33,27 +39,36 @@ const DeamHomePage = () => {
         }
     };
 
+    /* 쇼핑몰 픽 상품 클릭 이벤트를 실행하는 함수이다.
+     * 상세상품으로 가서 item 정보를 전달 해야되기 때문에 item을 인자로 받는다.
+     * item 타입 : ItemInterface */
     const onPickHandle = (item: ItemInterface) => {
         const encodedPublicId = btoa(item.publicId.toString()).slice(0, -1);
-        navigation(`/shop/product?publicId=${encodedPublicId}`, {
+        navigation(`${Path.product}?publicId=${encodedPublicId}`, {
             state: {
                 productItem: item,
             },
         });
     };
 
+    /* 쇼핑몰 인기 상품, 신상 상품 클릭 이벤트를 실행하는 함수이다.
+     * 상세상품으로 가서 item 정보를 전달 해야되기 때문에 item을 인자로 받는다.
+     * item 타입 : ItemInterface */
     const onProductHandle = (item: ItemInterface) => {
         const encodedPublicId = btoa(item.publicId.toString()).slice(0, -1);
-        navigation(`/shop/product?publicId=${encodedPublicId}`, {
+        navigation(`${Path.product}?publicId=${encodedPublicId}`, {
             state: {
                 productItem: item,
             },
         });
     };
 
+    /* 쇼핑몰 상품 리뷰 클릭 이벤트를 실행하는 함수이다.
+     * 상품 리뷰로 가서 item 리뷰 정보를 전달 해야되기 때문에 review를 인자로 받는다.
+     * review 타입 : ReviewResponse */
     const onReviewHandle = (review: ReviewResponse) => {
         const encodeReviewId = btoa(review.reviewId.toString()).slice(0, -1);
-        navigation(`/review?reviewId=${encodeReviewId}`, {
+        navigation(`${Path.review}?reviewId=${encodeReviewId}`, {
             state: {
                 userReview: review,
             },
