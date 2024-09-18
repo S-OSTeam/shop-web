27132d17d5ec5eab79323460e076b63d3b41aee5
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-09-18 15:21:22 +0900

    chore : 회원가입 user, email 중복 체크하는 mutation 추가

--- src/components/organisms/signup/form/Form.tsx
+++ src/components/organisms/signup/form/Form.tsx
@@ -28,7 +28,7 @@ const Form = ({ formInfo }: FormProps) => {
         email: '',
         phone: '',
         receiveMail: true,
-        sns: 'NAVER',
+        sns: '',
         userName: '',
     });
 
