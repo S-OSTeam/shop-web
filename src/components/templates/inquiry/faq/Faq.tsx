0f5c7e8988d80c3a932c19c9eb8ac68522289e04
Noma9363 <noma9363@gmail.com>
2024-09-26 17:27:45 +0900

    new : tinyMCE 리치 에디터 추가
    기존 ckeditor5 는 craco 관련 docs 가 존재하지 않거나 현재 지원하지 않는 메뉴얼 밖에 없어서 해당 플러그인과 컴포넌트 삭제
    
    tinyMCE 추가 : Inqueriy 컴포넌트에 임시로 작동되게 넣음
    
    - 추가할 사항
    이미지 업로드
    동영상 업로드
    시멘틱 태그 HTML 등
    XSS 취약점 보안 개선 등

--- src/components/templates/inquiry/faq/Faq.tsx
+++ src/components/templates/inquiry/faq/Faq.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import { Stack } from '@mui/material';
 import { Heading } from '@molecules/admin/layout/heading/Heading';
 import { AutoComplete } from '@molecules/autoComplete/AutoComplete';
-import { CustomCKEditor } from '@organisms/ckEditor/CKEditor';
-import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
 
 interface FaqProps {
     className?: string;
@@ -15,11 +13,6 @@ export const Faq = ({ ...props }: FaqProps) => {
     // 실행 준비 여부
     const [isReady, setIsReady] = React.useState(true);
 
-    // 에디터 ref 요소 접근
-    // 현재 DOM 에 접근한 에디터 요소 참조
-    // TODO : editor: unknown 말고 고민하기
-    const editorRef = React.useRef<{ editor: ClassicEditor | null }>({ editor: null });
-
     /* 변수 */
     /** AutoComplete > option 속성 변수 */
     const optionsCont = [
@@ -27,22 +20,8 @@ export const Faq = ({ ...props }: FaqProps) => {
         { label: 'two', number: 2 },
         { label: 'the', number: 3 },
     ];
-    // 한번만 마운트되고 에디터 초기화 이벤트
-    React.useEffect(() => {
-        setIsReady(true);
-        return () => {
-            // 에디터 인스턴스를 제거
-            if (editorRef.current.editor) {
-                editorRef.current.editor.destroy().catch((error: Error) => alert(`destory editor Instance : ${error}`));
-            }
-        };
-    }, []);
 
     /* 함수 */
-    // ckEditor 입력 이벤트
-    const handleEditorChange = (data: string) => {
-        console.log(`Editor content : ${data}`);
-    };
     /* TSX */
     /** Faq 헤드라인 컴포넌트 */
     const headline = <Heading heading="(FAQ)자주묻는 질문" subtitle1="자주 묻는 질문과 답변을 확인해 보세요" />;
@@ -51,13 +30,6 @@ export const Faq = ({ ...props }: FaqProps) => {
     return (
         <Stack>
             {headline}
-            <CustomCKEditor
-                isReady={isReady}
-                editorRef={editorRef}
-                initialData="write Here"
-                onChange={handleEditorChange}
-                editorConfig={{}}
-            />
             <AutoComplete options={optionsCont} freeSolo />
         </Stack>
     );
