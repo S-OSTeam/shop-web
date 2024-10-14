cecdf3a162c28db4ab2ab1f0afc8ed1a649340e7
dmltn <dmltn3426@naver.com>
2024-10-14 15:34:42 +0900

    design: 상세페이지

---
+++ src/components/templates/product/faq/ProductFAQ.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const ProductFAQ = () => {
+    return (
+        <Box>
+            <div />
+        </Box>
+    );
+};
+
+export default ProductFAQ;
---
+++ src/components/templates/product/review/ProductReview.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const ProductReview = () => {
+    return (
+        <Box>
+            <div />
+        </Box>
+    );
+};
+
+export default ProductReview;
