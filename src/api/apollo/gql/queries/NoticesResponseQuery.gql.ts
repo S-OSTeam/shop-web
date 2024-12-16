3b8e142aa7c8f11d06a130d11fb132006b986b21
Noma9363 <noma9363@gmail.com>
2024-12-16 14:40:53 +0900

    feat : 공지사항 이벤트 조회 API 그래파이큐엘 쿼리 추가
    - GET_EVENT_TYPE 이벤트 타입 목록을 조회하는 쿼리
    - GET_EVENT_LIST 이벤트 목록을 조회하는 쿼리
    - GET_EVENT_INFO 이벤트 목록을 조회한 id 값을 통해 이벤트 상세 조회 쿼리

--- src/api/apollo/gql/queries/NoticesResponseQuery.gql.ts
+++ src/api/apollo/gql/queries/NoticesResponseQuery.gql.ts
@@ -1,9 +1,44 @@
 import { gql } from '@apollo/client';
 
-export const SEARCH_NOTICE = gql`
-    query SearchNotice($request: NoticeSearchRequest!){
-        searchNotice(request: $request){
-            
+// 이벤트 타입들 조회
+export const GET_EVENT_TYPE = gql`
+    query GetEventType {
+        __typename(name: "EventType") {
+            enumValues {
+                name
+                description
+            }
+        }
+    }
+`;
+
+// 이벤트 목록 조회
+export const GET_EVENT_LIST = gql`
+    query GetEventList($eventType: EventType) {
+        getEventList(eventType: $eventType) {
+            id
+            startedAt
+            endedAt
+            title
+            contents
+            thumbnail
+        }
+    }
+`;
+
+// 이벤트 상세 조회
+export const GET_EVENT_INFO = gql`
+    query GetEventInfo($eventId: Long!) {
+        getEventInfo(eventId: $eventId) {
+            id
+            startedAt
+            endedAt
+            title
+            contents
+            thumbnail
+            items
+            images
+            link
         }
     }
 `;
