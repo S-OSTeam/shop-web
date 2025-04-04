89880805fb3cd40698b5030243b88bda2164f5c0
Choi Yong Won <cywin0821@gmail.com>
2025-04-04 17:16:34 +0900

    feat : Modal 몰큘, 인라인으로 작성된 scss className으로 변경

--- src/components/molecules/modal/Modal.tsx
+++ src/components/molecules/modal/Modal.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Fade, Modal as MuiModal, Box } from '@mui/material';
+import styles from './styles/Modal.module.scss';
 
 interface ModalProps {
     open: boolean;
@@ -13,37 +14,21 @@ interface ModalProps {
 }
 
 const Modal = ({ open, onClose, title, body, footer, children, className, style }: ModalProps) => {
+    const combinedStyle = useMemo<React.CSSProperties>(
+        () => ({
+            ...style,
+        }),
+        [style],
+    );
     return (
         <MuiModal open={open} onClose={onClose}>
             <Fade in={open}>
-                <Box
-                    className={className}
-                    style={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        backgroundColor: 'white',
-                        borderRadius: '8px',
-                        padding: '16px',
-                        outline: 'none',
-                        boxShadow: '0px 4px 16px rgba(0, 0, 0, 0.2)',
-                        width: '40%',
-                        maxWidth: '600px',
-                        minWidth: '300px',
-                        minHeight: '250px',
-                        maxHeight: '60vh',
-                        overflowY: 'auto',
-                        textAlign: 'center',
-                        fontSize: '1.2rem',
-                        ...style,
-                    }}
-                >
-                    {title && <h2 style={{ marginBottom: '16px', fontSize: '2rem' }}>{title}</h2>}
+                <Box className={`${styles.modalBox} ${className || ''}`} style={combinedStyle}>
+                    {title && <h2 className={styles.modalTitle}>{title}</h2>}
 
                     {body}
 
-                    {footer && <div style={{ marginTop: '16px' }}>{footer}</div>}
+                    {footer && <div className={styles.modalFooter}>{footer}</div>}
 
                     {children}
                 </Box>
