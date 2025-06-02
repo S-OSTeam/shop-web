a3260614c5c28cd7b8427e576fe60f2f1007c729
Choi Yong Won <cywin0821@gmail.com>
2025-06-02 11:18:25 +0900

    feat : 회원가입 페이지 및 템플릿에서 코드 되도록 수정

--- src/components/templates/signup/SignUpTemplate.tsx
+++ src/components/templates/signup/SignUpTemplate.tsx
@@ -1,6 +1,4 @@
-import React, { useState } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React from 'react';
 import { Box } from '@mui/material';
 import AccountInfoForm from '@organisms/signup/form/AccountInfoForm';
 import AgreementList from '@organisms/signup/List/AgreementList';
@@ -8,84 +6,47 @@ import UserInfoForm from '@organisms/signup/form/UserInfoForm';
 import clsN from 'classnames';
 import Button from '@atoms/button/Button';
 import Text from '@atoms/text/Text';
-import Modal from '@molecules/modal/Modal';
-import useGraphQL from '@hooks/useGraphQL';
-import { getCookie } from '@util/CookieUtil';
-import { SIGN_UP } from '@api/apollo/gql/mutations/LoginMutation.gql';
-import { signUpState } from '@recoil/atoms/signup/signupAtom';
-import { signupValidationState } from '@recoil/atoms/signup/signupValidationAtom';
+import { Modal, ModalButton, ModalActions } from '@molecules/modal';
 import style from './style/style.module.scss';
 
-const SignUpTemplate = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const snsValue = location.state?.sns || 'NORMAL';
-    const snsToken = snsValue === 'NAVER' || snsValue === 'KAKAO' ? getCookie('snsToken') : '';
+interface AccountInfoData {
+  userId: string;
+  pwd: string;
+  confirmPwd: string;
+  email: string;
+  phone: string;
+  receiveMail: boolean;
+}
 
-    const [signUpData, setSignUpData] = useRecoilState(signUpState);
+interface UserInfoData {
+  name: string;
+  birthDay: string;
+  sex: boolean;
+}
 
-    const [authModalOpen, setAuthModalOpen] = useState(false);
-    const [errorModalOpen, setErrorModalOpen] = useState(false);
+interface SignUpTemplateProps {
+  authModalOpen: boolean;
+  errorModalOpen: boolean;
+  isButtonEnabled: boolean;
+  onAccountInfoSubmit: (data: AccountInfoData) => void;
+  onUserInfoSubmit: (data: UserInfoData) => void;
+  onAgreementChange: (checked: boolean) => void;
+  onSignUp: () => void;
+  onCloseAuthModal: () => void;
+  onCloseErrorModal: () => void;
+}
 
-    const validationState = useRecoilValue(signupValidationState);
-    const isButtonEnabled =
-        validationState.isAccountValid && validationState.isUserValid && validationState.isAgreementChecked;
-
-    const { refetch } = useGraphQL({
-        query: SIGN_UP,
-        type: 'mutation',
-        request: signUpData,
-        option: {
-            'Authorization-mac': '2C-6D-C1-87-E0-B5',
-            ...(snsToken && { 'Authorization-SNS': snsToken }),
-        },
-    });
-
-    const handleAccountInfo = (accountData: {
-        userId: string;
-        pwd: string;
-        confirmPwd: string;
-        email: string;
-        phone: string;
-        receiveMail: boolean;
-    }) => {
-        console.log(accountData);
-    };
-
-    const handleUserInfo = (data: { name: string; birthDay: string; sex: boolean }) => {
-        const formattedBirthday = new Date(data.birthDay).toISOString();
-        setSignUpData((prev) => ({
-            ...prev,
-            userName: data.name,
-            birthday: formattedBirthday,
-            sex: data.sex,
-        }));
-    };
-
-    const signUpHandler = () => {
-        if (signUpData.birthday) {
-            try {
-                setSignUpData((prev) => ({
-                    ...prev,
-                    birthday: new Date(signUpData.birthday).toISOString(),
-                }));
-            } catch (error) {
-                setErrorModalOpen(true);
-                return;
-            }
-        }
-
-        console.log('요청 데이터:', signUpData);
-
-        refetch({
-            variables: {
-                input: signUpData,
-            },
-        })
-            .then(() => setAuthModalOpen(true))
-            .catch(() => setErrorModalOpen(true));
-    };
-
+const SignUpTemplate = ({
+  authModalOpen,
+  errorModalOpen,
+  isButtonEnabled,
+  onAccountInfoSubmit,
+  onUserInfoSubmit,
+  onAgreementChange,
+  onSignUp,
+  onCloseAuthModal,
+  onCloseErrorModal
+}: SignUpTemplateProps) => {
     return (
         <Box className={clsN(style['template-wrapper'])}>
             <Text
@@ -95,30 +56,42 @@ const SignUpTemplate = () => {
                 align="center"
             />
 
-            <AccountInfoForm formInfo={handleAccountInfo} />
+            <AccountInfoForm formInfo={onAccountInfoSubmit} />
 
-            <UserInfoForm onSubmit={handleUserInfo} />
+            <UserInfoForm onSubmit={onUserInfoSubmit} />
 
-            <AgreementList onChange={(checked) => setSignUpData((prev) => ({ ...prev, receiveMail: checked }))} />
+            <AgreementList onChange={onAgreementChange} />
 
             <Box className={clsN(`${style['template-wrapper__btn-wrapper']}`)}>
                 <Button
                     className={clsN(`${style['template-wrapper__btn-wrapper__btn']}`)}
-                    onClick={signUpHandler}
+                    onClick={onSignUp}
                     disabled={!isButtonEnabled}
                 >
                     회원가입
                 </Button>
             </Box>
 
-            <Modal open={authModalOpen} onClose={() => navigate('/')} title="회원가입 성공">
-                <p>회원가입이 완료되었습니다.</p>
-                <Button onClick={() => navigate('/')}>닫기</Button>
+            <Modal open={authModalOpen} onClose={onCloseAuthModal} title="회원가입 성공">
+                <div className={style.modalContent}>
+                    <p>회원가입이 완료되었습니다.</p>
+                    <ModalActions align="center">
+                        <ModalButton buttonVariant="primary" onClick={onCloseAuthModal}>
+                            홈으로 이동
+                        </ModalButton>
+                    </ModalActions>
+                </div>
             </Modal>
 
-            <Modal open={errorModalOpen} onClose={() => setErrorModalOpen(false)} title="회원가입 실패">
-                <p>제출양식을 확인하고 다시 입력해주세요.</p>
-                <Button onClick={() => setErrorModalOpen(false)}>닫기</Button>
+            <Modal open={errorModalOpen} onClose={onCloseErrorModal} title="회원가입 실패">
+                <div className={style.modalContent}>
+                    <p>제출양식을 확인하고 다시 입력해주세요.</p>
+                    <ModalActions align="center">
+                        <ModalButton buttonVariant="secondary" onClick={onCloseErrorModal}>
+                            닫기
+                        </ModalButton>
+                    </ModalActions>
+                </div>
             </Modal>
         </Box>
     );
--- src/pages/signup/SignUpPage.tsx
+++ src/pages/signup/SignUpPage.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { getCookie } from '@util/CookieUtil';
 import SignUpTemplate from '@templates/signup/SignUpTemplate';
+import { useSignUp } from '@hooks/signup/useSignUp';
 
 const SignUpPage = () => {
+    const location = useLocation();
+    const snsValue = location.state?.sns || 'NORMAL';
+    const snsToken = snsValue === 'NAVER' || snsValue === 'KAKAO' ? getCookie('snsToken') : '';
+    
+    const {
+        authModalOpen,
+        errorModalOpen,
+        isButtonEnabled,
+        handleAccountInfo,
+        handleUserInfo,
+        handleAgreementChange,
+        signUpHandler,
+        closeAuthModal,
+        closeErrorModal
+    } = useSignUp(snsToken);
+
     return (
         <Box component="div">
-            <SignUpTemplate />
+            <SignUpTemplate 
+                authModalOpen={authModalOpen}
+                errorModalOpen={errorModalOpen}
+                isButtonEnabled={isButtonEnabled}
+                onAccountInfoSubmit={handleAccountInfo}
+                onUserInfoSubmit={handleUserInfo}
+                onAgreementChange={handleAgreementChange}
+                onSignUp={signUpHandler}
+                onCloseAuthModal={closeAuthModal}
+                onCloseErrorModal={closeErrorModal}
+            />
         </Box>
     );
 };
